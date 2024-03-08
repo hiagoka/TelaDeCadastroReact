ff5bf62f505505288230ae8612e173728ba7310b
Hiago Kalil <125253798+hiagoka@users.noreply.github.com>
2024-03-08 10:52:14 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Imagem from './componentes/Imagem';
+import Login from './componentes/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Imagem />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
