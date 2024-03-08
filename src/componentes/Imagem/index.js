a6bf48b5a6794101cfd2217a669aceb385a15db0
Hiago Kalil <125253798+hiagoka@users.noreply.github.com>
2024-03-08 10:59:09 -0300

    Add files via upload

---
+++ src/componentes/Imagem/index.js
@@ -0,0 +1,11 @@
+import './imagem.css'
+
+function Imagem(){
+    return (
+    <header className="imagem">
+        <img src="/imagens/computador.png"  alt="Computador"/>
+    </header>
+    )
+}
+
+export default Imagem;
